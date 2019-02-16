5094c5639adcee60ac3d9af3ee6d77ad82fb6b22
sapeleD3 <oyinkuromosesvictor@outlook.com>
2019-02-16 05:24:40 -0800

    first commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React,  { Component } from 'react';
+
+class Card extends Component{
+    render(){
+        return (
+            <div>
+                <img src="" alt="roboPhoto"/>
+                <div>
+                    <h2>Sapele Moses</h2>
+                    <p>oyinkuromosesvictor@gmail.com</p>
+                </div>
+            </div>
+        );
+    }
+}
+export default Card;
